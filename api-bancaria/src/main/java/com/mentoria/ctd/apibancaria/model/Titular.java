eec127b29f8103ee9e7f28524f32a08d8478a671
Ualace36 <89046095+Ualace36@users.noreply.github.com>
2022-10-16 16:11:53 -0300

    Estreutura inicial do Projeto

---
+++ api-bancaria/src/main/java/com/mentoria/ctd/apibancaria/model/Titular.java
@@ -0,0 +1,60 @@
+package com.mentoria.ctd.apibancaria.model;
+
+public class Titular {
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    Endereco endereco;
+
+    public Titular() {
+    }
+
+    public Titular(Long id, String nome, String sobrenome, String cpf, Endereco endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
